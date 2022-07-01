abd2955e9500bbb4f1d6c974a3e9105f591c1ea3
Cauã Esteves <cauaesteves482@gmail.com>
2022-07-01 19:44:43 -0300

    Aula 05

--- assets/scripts/condicionais.js
+++ assets/scripts/condicionais.js
@@ -38,6 +38,75 @@ function parOuImpar() {
 
 function calcularCombustiveis() {
     const precoEtanol = document.getElementById("txtEtanol").value;
-    const precoGasolina = document.getElementById("txtEtanol").value;
+    const precoGasolina = document.getElementById("txtGasolina").value;
+    var frase;
+    const resultado = document.getElementById("resultado");
+
+    const precoEtanolArrumado= precoEtanol.replace(",",".");
+    const precoGasolinaArrumado= precoGasolina.replace(",",".");
+
+    if (precoEtanolArrumado/precoGasolinaArrumado > 0.7) {
+        frase = "Abasteça com Gasolina!";
+    }
+    else if(precoEtanolArrumado/precoGasolinaArrumado < 0.7) {
+        frase = "Abasteça com Etanol!";
+    }
+    else {
+        frase = "Você pode escolher qualquer um dos dois!";
+    }
+
+    resultado.innerHTML = frase;
+    resultado.style.display = "block";
 }
-    //Continuar aqui...
+
+function verificarBeneficio() {
+    const salario = document.getElementById("txtSalario").value;
+    const anoInicio = document.getElementById("txtAnoInicio").value;
+    const anoAtual = new Date().getFullYear();
+    var beneficio, frase;
+    const resultado = document.getElementById("resultado");
+
+    if (salario < 2200 && anoInicio < anoAtual - 10) {
+        beneficio = salario * 0.1;
+        frase = "<p>O valor do benefício será " + beneficio + "</p>";
+    }
+
+    else if(salario < 2200 || anoInicio < anoAtual - 10) {
+        beneficio = salario * 0.05;
+        frase = "<p>O valor do benefício será " + beneficio + "</p>";
+    }
+
+    else [
+        frase = "<p>O funcionário não tem direito ao benefício</p>"
+    ]
+
+    resultado.innerHTML = frase;
+    resultado.style.display = "block";
+}
+
+function verificarBeneficioFilhos() {
+    const salario = document.getElementById("txtSalario").value;
+    const anoInicio = document.getElementById("txtAnoInicio").value;
+    const anoAtual = new Date().getFullYear();
+    const numeroFilhos = document.getElementById("txtFilhos").value;
+    var beneficio, frase;
+    const resultado = document.getElementById("resultado");
+
+    if ((salario < 2200 && anoInicio < anoAtual - 10) || filhos != 0) {
+        const acrescimo = numeroFilhos * salario * 0.01;
+        beneficio = salario * 0.1 + acrescimo;
+        frase = "<p>O valor do benefício será " + beneficio + "</p>";
+    }
+
+    else if(salario < 2200 || anoInicio < anoAtual - 10) {
+        beneficio = salario * 0.05;
+        frase = "<p>O valor do benefício será " + beneficio + "</p>";
+    }
+
+    else [
+        frase = "<p>O funcionário não tem direito ao benefício</p>"
+    ]
+
+    resultado.innerHTML = frase;
+    resultado.style.display = "block";
+}
